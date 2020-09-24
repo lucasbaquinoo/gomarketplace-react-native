d28cea703e6098a1f4c5ef72674b3969eb7ab7f6
lucasbaquino <lucasbaquino@icloud.com>
2020-09-24 17:13:04 -0300

    All tests passed

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,26 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
-
-    return formatValue(0);
+    const total = products.reduce(
+      (accumulator, product) => {
+        accumulator.count += product.price * product.quantity;
+        return accumulator;
+      },
+      { count: 0 },
+    );
+    return formatValue(total.count);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const quantity = products.reduce(
+      (accumulator, product) => {
+        accumulator.count += product.quantity;
+        return accumulator;
+      },
+      { count: 0 },
+    );
 
-    return 0;
+    return quantity.count;
   }, [products]);
 
   return (
